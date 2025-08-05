e910aea3597b1a7451ceb73df6cb7195b487cfc2
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-08-05 15:19:06 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import gsap from "gsap"
+import _ScrollTrigger from "gsap/ScrollTrigger"
+import _SplitText from "gsap/SplitText"
+
+gsap.registerPlugin(_ScrollTrigger, _SplitText)
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
