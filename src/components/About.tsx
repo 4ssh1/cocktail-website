282c8c41f88efc1d27530f990957f9a12be409cc
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-09-14 18:50:10 +0100

    completed about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,42 @@
+import { useGSAP } from "@gsap/react"
+import { SplitText, gsap } from "gsap/all"
 
 const About = () => {
+  useGSAP(()=> {
+    const titleSplit = SplitText.create('#about h1', {
+      type: "words"
+    })
+
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: 'top center'
+      }
+    })
+
+    scrollTimeline.from(titleSplit.words, {
+      opacity: 0,
+      duration: 1,
+      yPercent: 100,
+      ease: "expo.out",
+      stagger: 0.07
+    }).from(".top-grid div, .bottom-grid div", {
+      opacity: 0, 
+      duration: 1,
+      ease: "power1.inOut",
+      stagger: 0.04
+    }, '-=0.5')
+
+  })
+
   return (
     <section id="about">
-      <div className="mb-16 md:px-0 px-5">
+      <div className="mb-16 md:px-0 px-5 relative">
+        <div className="bg-black absolute top-0 right-0 opacity-80"></div>
         <div className="content">
             <div className="md:col-span-8 pt-10">
-                <p className="badge exo-2 bg-white py-1 px-1.5 rounded-2xl inline text-black text-sm">Best Cocktails</p>
-                <h1 className="paytone pt-5">Where every details matters <span className="text-white">- </span>
+                <p className="badge exo-2 bg-white py-1 px-2.5 rounded-2xl inline text-black text-xs sm:text-sm">Best Cocktails</p>
+                <h1 className="paytone pt-5 text-3xl pb-2">Where every details matters <span className="text-white">- </span>
                     from muddle to garnish
                 </h1>
             </div>
@@ -16,31 +46,31 @@ const About = () => {
               </p>
 
               <div>
-                <p className="md:text-3xl text-xl font-bold">
+                <p className="md:text-3xl text-xl font-bold py-2">
                   <span>4.5</span>/5
                 </p>
-                <p className="text-sm text-white">
+                <p className="text-xs sm:text-sm text-white">
                   More than +1200 customers
                 </p>
               </div>
             </div>
 
-            <div className="top-grid grid grid-cols-12 gap-4">
-              <div className="md:col-span-3 h-48 relative">
+            <div className="top-grid grid grid-cols-1 sm:grid-cols-6 lg:grid-cols-12 gap-4 pt-10">
+              <div className="sm:col-span-3 lg:col-span-3 h-48 relative">
                 <img 
                   src="drink-3.jpg" 
                   alt="grid-img-1" 
                   className="absolute inset-0 w-full h-full object-cover rounded-lg" 
                 />
               </div>
-              <div className="md:col-span-6 h-48 relative">
+              <div className="sm:col-span-3 lg:col-span-6 h-48 relative">
                 <img 
                   src="drink-5.jpg" 
                   alt="grid-img-1" 
                   className="absolute inset-0 w-full h-full object-cover rounded-lg" 
                 />
               </div>
-              <div className="md:col-span-3 h-48 relative">
+              <div className="sm:col-span-6 lg:col-span-3 h-48 relative">
                 <img 
                   src="drink-6.jpg" 
                   alt="grid-img-1" 
@@ -49,17 +79,17 @@ const About = () => {
               </div>
             </div>
 
-            <div className="bottom-grid grid grid-cols-12 gap-4 mt-4">
-              <div className="md:col-span-8 h-48 relative">
+            <div className="bottom-grid grid grid-cols-1 sm:grid-cols-6 lg:grid-cols-12 gap-4 mt-4">
+              <div className="sm:col-span-4 lg:col-span-8 h-48 relative">
                 <img 
                   src="drink-4.jpg" 
                   alt="grid-img-1" 
                   className="absolute inset-0 w-full h-full object-cover rounded-lg" 
                 />
               </div>
-              <div className="md:col-span-4 h-48 relative">
+              <div className="sm:col-span-2 lg:col-span-4 h-48 relative">
                 <img 
-                  src="drink-4.jpg" 
+                  src="abt1.png" 
                   alt="grid-img-1" 
                   className="absolute inset-0 w-full h-full object-cover rounded-lg" 
                 />
